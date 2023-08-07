88d84b7563cc3f499c1f4034c4b4066921f370c6
onurd <106928484+onurdzd@users.noreply.github.com>
2023-08-07 12:46:50 +0300

    solved

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import Model.GroceryList;
+
+import java.util.*;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        GroceryList marketList1=new GroceryList();
+        boolean working=true;
+
+        while (working) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println(0 + " Uygulamayı durdur");
+            System.out.println(1 + " Eleman ekle");
+            System.out.println(2 + " Eleman çıkar");
+            String inputText = scanner.nextLine();
+            if (inputText.equals("0")) {
+                System.out.println(" Uygulama durduruldu.");
+                working=false;
+            } else if (inputText.equals("1")) {
+                System.out.println(" Eklenmesini istediğiniz elemanları giriniz.");
+                String inputText1 = scanner.nextLine();
+                marketList1.addItems(inputText1);
+            } else if (inputText.equals("2")) {
+                System.out.println(" Cıkarılmasını istediğiniz elemanları giriniz.");
+                String inputText2 = scanner.nextLine();
+                marketList1.removeItems(inputText2);
+            } else {
+                System.out.println("Hatalı değer girildi");
+            }
+        }
     }
 }
